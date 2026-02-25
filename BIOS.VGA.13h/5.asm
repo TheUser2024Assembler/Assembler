1a4e46df502442bec15b61628c612d2084951749
TheUser2024 <hacker.user.anon@proton.me>
2026-02-25 16:25:11 +0300

    Create 5.asm

---
+++ BIOS.VGA.13h/5.asm
@@ -0,0 +1,44 @@
+use16
+org 0x7C00
+count:mov ax,0x0013
+int 0x10
+mov ax,0xA000
+mov es,ax
+xor di,di
+mov cx,32000
+xor ax,ax
+rep stosw
+mov byte[count+1],0
+mov byte[count+2],0
+mov byte[count],4
+s:call d
+add byte[x],8
+dec byte[count]
+jnz s
+jmp $
+c:db 0xAA,0x55,0xAA,0x55,0xAA,0x55,0xAA,0x55
+d:mov si,c
+movzx bx,byte[count+1]
+movzx ax,byte[count+2]
+mov cx,320
+imul bx,cx
+add bx,ax
+mov di,bx
+mov cx,8
+nxtln:lodsb
+push cx
+mov cx,8
+mov ah,al
+nxtpx:shl ah,1
+jnc skp
+mov al,0x28
+mov[es:di],al
+mov al,ah
+skp:inc di
+loop nxtpx
+add di,320-8
+pop cx
+loop nxtln
+ret
+times 510-($-$$) db 0
+dw 0xAA55
